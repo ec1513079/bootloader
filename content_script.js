a0dcb0b1cdf813a9e018824d7f4385e7a3785013
Kotaro Takahashi <kotakahashi@tech.softbank.co.jp>
2013-01-21 11:56:52 +0900

    Impl : unlock request in content script

--- content_script.js
+++ content_script.js
@@ -1,39 +1,22 @@
 
-function rap_pass(pass)
+function requestRapPass(pass)
 {
-	alert("rap pass : " + pass);
+	console.log("rap pass : " + pass);
 	document.getElementsByName('exp_password')[0].setAttribute("value", pass);
+};
+
+function requestUnlockPage(url) {
+	console.log("unlock url : " + url);
+	chrome.extension.sendRequest({ action : "request_unlock_page", url : url }, function(response) {
+		console.log("unlock response : " + response);
+	});
 }
 
-$(function(){
-	
+$(function() {
 	acces = document.getElementById('acces').getElementsByTagName("a")[0];
-	if(acces.innerText == "URLロック元に戻る")
-	{
+	if(acces.innerText == "URLロック元に戻る") {
 		rap_pass("hogehoge");
+	} else {
+		requestUnlockPage(acces.innerText);
 	}
-	else
-	{
-		chrome.extension.sendRequest({ action : "bootloader_setting_enable" }, function(response) {
-			
-			if (response.value == "enable") {
-        		var link_ = $("a.l:first").attr("href");
-
-        		var xhr_ = new XMLHttpRequest();
-        		xhr_.open("GET", link_, true);
-        		xhr_.onreadystatechange = function() {
-        		  if (xhr_.readyState == 4) {
-        		    var resp_ = xhr_.responseText;
-
-        		    document.location = link_;
-        			console.log("document.location = "+link_);
-
-        			console.log("END");
-        		  }
-        		}
-        		xhr_.send();
-        		console.log("XMLHttpRequest Send");
-        	}
-    	});
-    }
 });
