09015c656c0998c40df927148881ec3904a4d3cf
Kotaro Takahashi <kotakahashi@tech.softbank.co.jp>
2013-01-21 16:33:40 +0900

    Impl : option page inner script

--- options.js
+++ options.js
@@ -4,6 +4,10 @@ function initSetting() {
 	if(!localStorage["setting_bootloader_enable"]) {
 		localStorage["setting_bootloader_enable"] = "enable";
 	}
+
+	if(!localStorage["setting_bootloader_pass_list"]) {
+		localStorage["setting_bootloader_pass_list"] = JSON.stringify([]);
+	}
 };
 
 
@@ -33,7 +37,38 @@ function isSettingBootloaderEnable() {
 
 
 /************************************************
- * Kick Start
+ * RapPass List
  ************************************************/
 
+
+
+/************************************************
+ * Option Page Inner Script
+ ************************************************/
+
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+$(document).ready(function(){
+	if(getUrlVars()["page_action"] == "true") {
+		$("td.url_row, td.key_row").each(function(){
+			var url_ = $(this).text() ;
+		    if(url_.length > 40) { $(this).text(url_.substring(0,40) + "..."); }
+		});
+	}
+});
+
+
+/************************************************
+ * Kick Start
+ ************************************************/
 initSetting();
